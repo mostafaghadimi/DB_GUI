041f42a145b83ebffe34dd32a897ab1f515f5255
Mostafa <mostafa.ghadimi@yahoo.com>
2019-01-26 18:00:48 +0330

    upload project files

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 var express = require('express')
 var app = express()
 var path = require('path')
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false })) 
+app.use(bodyParser.json())
+
+var pg = require('pg')
+
 app.use('/public', express.static(path.join(__dirname, '../public/')))
 const PORT = 8080
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/html/index.html'))
+})
+
+app.get('/product', (req, res) => {
+    res.send(req.query.product)
+})
+
+app.get('/seller', (req, res) => {
+    res.send(req.query.seller)
+})
+
+app.get('/user', (req, res) => {
+    res.send(req.query.user)
 })
 
 app.listen(PORT, () => {
