4121c5197e204fbb0ba345da08e24d09f23dbab6
mehrdad76 <mehrdadkarrabi1997@gmail.com>
2019-01-26 15:27:47 +0330

    new new new

--- server/server.js
+++ server/server.js
@@ -6,7 +6,16 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false })) 
 app.use(bodyParser.json())
 
-var pg = require('pg')
+const pg = require('pg');
+
+const config = {
+    user: 'postgres',
+    database: 'market',
+    password: '1',
+    port: 5432
+};
+
+const pool = new pg.Pool(config);
 
 // TODO: setup connection and query to database in the requests => Mehrdad
 
@@ -18,8 +27,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/product', (req, res) => {
-    res.send(req.query.product)
-})
+   pool.connect(function (err, client, done) {
+       if (err) {
+           console.log("Can not connect to the DB" + err);
+       }
+       client.query('(select * from product where name = \'' + req.query.product + '\') union (select * from product where name like \'' + req.query.product + '\') union (select p.id, p.sell_count, p.name, p.category_id, p.review from product p inner join category c on c.id = p.category_id where c.id in (select category_id from product where product.name like \'\\' + req.query.product + '\'))', function (err, result) {
+            done();
+            if (err) {
+                console.log("error1", err);
+                res.status(400).send(err);
+            }
+            res.status(200).send(result.rows);
+       })
+   })
+});
 
 app.get('/seller', (req, res) => {
     res.send(req.query.seller)
